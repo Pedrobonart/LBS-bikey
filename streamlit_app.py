e1b124eab17a6257b082707b1b480aef58bddca9
TimPauly <85361259+TimPauly@users.noreply.github.com>
2025-06-25 11:12:17 +0200

    Update streamlit_app.py vis

--- streamlit_app.py
+++ streamlit_app.py
@@ -271,7 +271,7 @@ elif page == "Analysis: Balance":
                 color = "gray"
 
             # Größe proportional zur Differenz (mit Mindestgröße)
-            radius = max(4, min(20, abs(diff)))
+            radius = max(4, min(15, abs(diff)/2))
 
             popup_text = f"""
             <b>{row['station']}</b><br>
