00048959bb1083a0caf06c226715806a1baa557f
TimPauly <85361259+TimPauly@users.noreply.github.com>
2025-06-25 12:09:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -295,12 +295,12 @@ elif page == "Analysis: Balance":
         st.metric("Most Imbalanced Station", 
                   balance_df.loc[balance_df["diff"].abs().idxmax(), "station"])
         st.metric("Total Stations", len(balance_df))
-        st.metric("Perfectly Balanced", (balance_df["diff"] == 0).sum())
+        
         more_departures = (balance_df["diff"] > 0).sum()
         more_arrivals = (balance_df["diff"] < 0).sum()
         st.markdown(f"**🟩 More Departures:** **123 stations**")
         st.markdown(f"**🟦 More Arrivals:** **118 stations**")
-
+        st.metric("Perfectly Balanced", (balance_df["diff"] == 0).sum())
 
 elif page == "Analysis: Network":
     st.header("Connectiveness of Stations")
