f5e54a88b8e04b3fa11cbd53296f4e7939de2f11
TimPauly <85361259+TimPauly@users.noreply.github.com>
2025-06-25 12:00:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -148,7 +148,7 @@ elif page == "Analysis: Heatmap":
         heat_points = heat_data[["lat", "lon", "count"]].values.tolist()
 
         # Heatmap erzeugen
-        map_center = [16.37018, 48.20950]
+        map_center = [48.20950, 16.37018]
         heat_map = folium.Map(location=map_center, zoom_start=11, min_zoom=10)
         HeatMap(heat_points, radius=15, blur=20, max_zoom=1).add_to(heat_map)
 
