67b6f67881aae745db24566e4e97b71985335ac8
Pedrobonart <65249286+Pedrobonart@users.noreply.github.com>
2025-06-26 16:34:28 +0200

    Create conclusion.py

---
+++ pages/conclusion.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def show_page():
+    st.header("Conclusion & Recommendations")
+    st.write("""
+        Based on the data, we can identify optimization potential in the network.
+        For example, we propose additional stations in underserved areas.
+    """)
